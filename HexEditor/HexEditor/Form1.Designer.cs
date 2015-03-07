e78d55529ac678484312993cc55cca7add918fde
ricardopetrere <ricardo19.93@hotmail.com>
2015-03-07 19:42:11 -0300

    Adicionado projeto de editor de arquivo .hex

---
+++ HexEditor/HexEditor/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace HexEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtCaminhoArquivo = new System.Windows.Forms.TextBox();
+            this.btnBuscarArquivo = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.colVariavel = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colTipo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colValor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 58);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(688, 114);
+            this.textBox1.TabIndex = 0;
+            // 
+            // txtCaminhoArquivo
+            // 
+            this.txtCaminhoArquivo.Location = new System.Drawing.Point(64, 23);
+            this.txtCaminhoArquivo.Name = "txtCaminhoArquivo";
+            this.txtCaminhoArquivo.ReadOnly = true;
+            this.txtCaminhoArquivo.Size = new System.Drawing.Size(555, 20);
+            this.txtCaminhoArquivo.TabIndex = 1;
+            // 
+            // btnBuscarArquivo
+            // 
+            this.btnBuscarArquivo.Location = new System.Drawing.Point(625, 12);
+            this.btnBuscarArquivo.Name = "btnBuscarArquivo";
+            this.btnBuscarArquivo.Size = new System.Drawing.Size(75, 40);
+            this.btnBuscarArquivo.TabIndex = 2;
+            this.btnBuscarArquivo.Text = "Buscar Arquivo";
+            this.btnBuscarArquivo.UseVisualStyleBackColor = true;
+            this.btnBuscarArquivo.Click += new System.EventHandler(this.btnBuscarArquivo_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colVariavel,
+            this.colTipo,
+            this.colValor});
+            this.dataGridView1.Location = new System.Drawing.Point(13, 179);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(687, 163);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // colVariavel
+            // 
+            this.colVariavel.HeaderText = "Variável";
+            this.colVariavel.Name = "colVariavel";
+            this.colVariavel.ReadOnly = true;
+            // 
+            // colTipo
+            // 
+            this.colTipo.HeaderText = "Tipo";
+            this.colTipo.Name = "colTipo";
+            this.colTipo.ReadOnly = true;
+            // 
+            // colValor
+            // 
+            this.colValor.HeaderText = "Valor";
+            this.colValor.Name = "colValor";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Arquivo:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(712, 354);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnBuscarArquivo);
+            this.Controls.Add(this.txtCaminhoArquivo);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtCaminhoArquivo;
+        private System.Windows.Forms.Button btnBuscarArquivo;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colVariavel;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colTipo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colValor;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ HexEditor/HexEditor/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HexEditor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// caractere que irá separar as linhas. Escolhi '~' (126), mas poderia ser '{' (123)
+        /// </summary>
+        char char_delimitador = (char)126;
+        private void btnBuscarArquivo_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                txtCaminhoArquivo.Text = openFileDialog1.FileName;
+
+                //Resetando layouts
+                textBox1.ResetText();
+                dataGridView1.Rows.Clear();
+
+                //Lendo bytes do arquivo
+                String texto = File.ReadAllText(openFileDialog1.FileName);
+                //Preenchendo textbox
+                foreach (char c_caractere in texto)
+                {
+                    textBox1.Text += c_caractere;
+                }
+
+                //Complicando um pouco. "Entendendo" os hex
+                String[] s_parametros = texto.Split(char_delimitador);
+                foreach (String s_parametro in s_parametros)
+                {
+                    //Devido ao modo como eu separo as linhas, é necessário remover a linha em branco do tratamento
+                    if(s_parametro.Length>0)
+                    {
+                        //Colunas do DataGridView
+                        String s_nome_variavel, s_tipo, s_valor;
+                        //Variável para "andar" na string, pegando os valores para por nas variáveis
+                        //É necessário verificar se o caractere '~' está presente, para evitar erros
+                        int index = 0 + (char_delimitador != '~' ? 1 : 0);
+
+                        //1º passo: Nome da Variável
+                        //Tamanho do nome da variável
+                        int i_tamanho_nome_variavel = (int)s_parametro[index];
+
+                        //Atribuindo o nome da variável
+                        s_nome_variavel = s_parametro.Substring(index + 1, i_tamanho_nome_variavel);
+
+                        //Somando em index o byte de tamanho da variável, mais o tamanho em si
+                        index += 1 + i_tamanho_nome_variavel;
+
+                        //2º passo: Valor
+                        //Descobrindo o tamanho do valor da variável. Números são salvos de trás pra frente
+                        int i_tamanho_valor = 0;
+                        for(int n = 0;n<3;n++)
+                        {
+                            i_tamanho_valor += (((int)Math.Pow(16, n)) * ((int)s_parametro[index + n]));
+                        }
+
+                        //Somando em index
+                        index += 3;
+
+                        //Pular delimitador nulo
+                        index += 1;
+
+
+                        /*
+                         * A variável i_tamanho_valor salva o tamanho do valor da variável, mas inclui o tipo de valor
+                         * e o delimitador nulo. É necessário tirar esses dois caracteres do valor, para não dar erro
+                         */
+                        i_tamanho_valor -= 2;
+
+                        //Tipo do valor
+                        switch ((int)s_parametro[index])
+                        {
+                            case 6://Datas
+                                s_tipo = "Data";
+                                s_valor = s_parametro.Substring(index + 1, i_tamanho_valor);
+                                //Encoding.Default.GetBytes(s_valor).ToString();
+                                //DateTime d = DateTime.FromBinary(Convert.ToInt64(s_valor, 16));
+                                break;
+                            case 9://Booleanos
+                                s_tipo = "Booleano";
+                                s_valor = (int)s_parametro[index + 1] == 1 ? "True" : "False";
+                                break;
+                            case 81://Vetores
+                                s_tipo = "Vetor";
+                                //É outro nível de cálculo
+                                s_valor = s_parametro.Substring(index + 1, i_tamanho_valor);
+                                break;
+                            case 11://Vive puxando 2 indexes além do necessário, dando erro
+                                s_tipo = "Index " + 11;
+                                s_valor = s_parametro.Substring(index + 1, i_tamanho_valor - 2);
+                                break;
+                            default:
+                                s_tipo = "Desconhecido - Index " + (int)s_parametro[index];
+                                s_valor = s_parametro.Substring(index + 1, i_tamanho_valor);    
+                                //throw new NotImplementedException("Tipo com index " + s_parametro[index].ToString() + " não conhecido. Necessário implementar");
+                                break;
+                        }
+                        //Somando em index
+                        index += 1 + i_tamanho_valor;
+
+                        /*A essa altura, já deve ter chego no final ('{' ou nada, caso o caractere delimitador escolhido tenha sido esse)
+                         * Está na hora de jogar esses dados no DataGridView
+                         */
+                        dataGridView1.Rows.Add(s_nome_variavel, s_tipo, s_valor);
+                    }
+                }
+            }
+        }
+    }
+}
