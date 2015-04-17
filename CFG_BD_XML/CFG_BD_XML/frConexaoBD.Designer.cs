e8f7303741c4890e784697b71825d9ae53b97aee
ricardopetrere <ricardo19.93@hotmail.com>
2015-04-17 20:58:33 -0300

    17_04

--- CFG_BD_XML/CFG_BD_XML/frConexaoBD.Designer.cs
+++ CFG_BD_XML/CFG_BD_XML/frConexaoBD.Designer.cs
@@ -115,6 +115,7 @@
             this.txtSenha.Name = "txtSenha";
             this.txtSenha.Size = new System.Drawing.Size(136, 20);
             this.txtSenha.TabIndex = 6;
+            this.txtSenha.UseSystemPasswordChar = true;
             // 
             // label3
             // 
