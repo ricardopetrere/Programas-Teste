e512e7e8ffc3250764c166ff3ca23d0a1f5a91f3
ricardopetrere <ricardo19.93@hotmail.com>
2016-04-20 11:42:50 -0300

    20_02: Adicionado projeto de Cálculo de soma de PG

---
+++ CalculosPG/CalculosPG/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace CalculosPG
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtRazao = new System.Windows.Forms.TextBox();
+            this.txtQuantidadeTermos = new System.Windows.Forms.TextBox();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPrimeiroTermo = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = global::CalculosPG.Properties.Resources.formula_soma_termos_pg;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(186, 94);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(86, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Fórmula:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(201, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Razão:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(262, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Nº de termos:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(201, 87);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Resultado:";
+            // 
+            // txtRazao
+            // 
+            this.txtRazao.Location = new System.Drawing.Point(204, 25);
+            this.txtRazao.Name = "txtRazao";
+            this.txtRazao.Size = new System.Drawing.Size(48, 20);
+            this.txtRazao.TabIndex = 0;
+            this.txtRazao.KeyDown += new System.Windows.Forms.KeyEventHandler(this.PressionarEnter);
+            // 
+            // txtQuantidadeTermos
+            // 
+            this.txtQuantidadeTermos.Location = new System.Drawing.Point(283, 25);
+            this.txtQuantidadeTermos.Name = "txtQuantidadeTermos";
+            this.txtQuantidadeTermos.Size = new System.Drawing.Size(50, 20);
+            this.txtQuantidadeTermos.TabIndex = 1;
+            this.txtQuantidadeTermos.KeyDown += new System.Windows.Forms.KeyEventHandler(this.PressionarEnter);
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(204, 103);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.ReadOnly = true;
+            this.txtResultado.Size = new System.Drawing.Size(191, 20);
+            this.txtResultado.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(201, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Primeiro Termo:";
+            // 
+            // txtPrimeiroTermo
+            // 
+            this.txtPrimeiroTermo.Location = new System.Drawing.Point(204, 64);
+            this.txtPrimeiroTermo.Name = "txtPrimeiroTermo";
+            this.txtPrimeiroTermo.Size = new System.Drawing.Size(129, 20);
+            this.txtPrimeiroTermo.TabIndex = 2;
+            this.txtPrimeiroTermo.KeyDown += new System.Windows.Forms.KeyEventHandler(this.PressionarEnter);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(339, 12);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(56, 72);
+            this.btnCalcular.TabIndex = 3;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(409, 134);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPrimeiroTermo);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.txtQuantidadeTermos);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtRazao);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtRazao;
+        private System.Windows.Forms.TextBox txtQuantidadeTermos;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPrimeiroTermo;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ CalculosPG/CalculosPG/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CalculosPG
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void CalcularPG()
+        {
+            /*
+             * Fórmula:
+             * 
+             * Sn = a*(q^n - 1)/(q-1),
+             * onde a = termo inicial, q = razão, n = nº de termos
+             */
+            double sn = 0;
+            double a = 0;
+            double q = 0;
+            int n = 0;
+            bool valido = true;
+            //System.Threading.Thread.CurrentThread.CurrentCulture.NumberFormat.NumberDecimalSeparator
+            // ou
+            //System.Globalization.CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator
+            if (!double.TryParse(txtPrimeiroTermo.Text, out a))
+            {
+                valido = false;
+            }
+            if (!double.TryParse(txtRazao.Text, out q))
+            {
+                valido = false;
+            }
+            if (!int.TryParse(txtQuantidadeTermos.Text, out n))
+            {
+                valido = false;
+            }
+            if (valido)
+            {
+                sn = a * ((Math.Pow(q, n) - 1) / (q - 1));
+                txtResultado.Text = sn.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Dados inválidos!");
+            }
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            CalcularPG();
+        }
+
+        private void PressionarEnter(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode==Keys.Enter)
+            {
+                CalcularPG();
+            }
+        }
+    }
+}
