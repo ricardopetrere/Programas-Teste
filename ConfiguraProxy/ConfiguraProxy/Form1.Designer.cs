bc07b0ffe9b65da58fbc48ab213dfa4fa11563e2
ricardopetrere <ricardopetrere@github.com>
2016-08-04 01:46:10 -0300

    Adicionado projeto de tela para configuração básica de Proxy

---
+++ ConfiguraProxy/ConfiguraProxy/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace ConfiguraProxy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Enabled = false;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(12, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(141, 24);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Proxy Habilitado";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IP:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 69);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(141, 26);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "127.0.0.1";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(155, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Porta:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Enabled = false;
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown1.Location = new System.Drawing.Point(159, 69);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(71, 26);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            3128,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(242, 120);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Configurar Proxy";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ ConfiguraProxy/ConfiguraProxy/Form1.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ConfiguraProxy
+{
+    public partial class Form1 : Form
+    {
+        const String SUBKEY = "Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings";
+        const String PROXYENABLE = "ProxyEnable";
+        const String PROXYSERVER = "ProxyServer";
+        const char C_DIVISOR = ':';
+        public RegistryKey registro;
+        String[] server;
+        private bool _permiteAlterar;
+        public bool PermiteAlterar
+        {
+            get
+            {
+                return _permiteAlterar;
+            }
+            set
+            {
+                _permiteAlterar = value;
+                checkBox1.Enabled = value;
+                textBox1.Enabled = value;
+                numericUpDown1.Enabled = value;
+            }
+        }
+
+        private void DesligaEventos()
+        {
+            //"Desliga" os eventos
+            checkBox1.CheckedChanged -= checkBox1_CheckedChanged;
+            textBox1.TextChanged -= textBox1_TextChanged;
+            numericUpDown1.ValueChanged -= numericUpDown1_ValueChanged;
+        }
+
+        private void LigaEventos()
+        {
+            //"Religa" os eventos
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            textBox1.TextChanged += textBox1_TextChanged;
+            numericUpDown1.ValueChanged += numericUpDown1_ValueChanged;
+        }
+
+        void LeDados()
+        {
+            //registro = Registry.CurrentUser.OpenSubKey(SUBKEY, PermiteAlterar);
+            checkBox1.Checked = (int)registro.GetValue(PROXYENABLE) > 0;
+            server = registro.GetValue(PROXYSERVER).ToString().Split(C_DIVISOR);
+            numericUpDown1.Value = Convert.ToInt16(server[1]);
+            textBox1.Text = server[0];
+            //registro.Close();
+        }
+
+        void GravarRegistro(String chave, object valor, RegistryValueKind tipo)
+        {
+            if(registro != null)
+            {
+                registro.SetValue(chave, valor, tipo);
+            }
+        }
+
+        void GravarProxyServer()
+        {
+            GravarRegistro(PROXYSERVER, server[0] + C_DIVISOR + server[1], RegistryValueKind.String);
+            //registro.SetValue(PROXYSERVER, server[0] + C_DIVISOR + server[1], RegistryValueKind.String);
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            GravarRegistro(PROXYENABLE, Convert.ToInt16(checkBox1.Checked), RegistryValueKind.DWord);
+            //registro.SetValue(PROXYENABLE, Convert.ToInt16(checkBox1.Checked), RegistryValueKind.DWord);
+            //Console.WriteLine("CheckedChanged");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //http://stackoverflow.com/questions/197725/programmatically-set-browser-proxy-settings-in-c-sharp
+            try
+            {
+                registro = Registry.CurrentUser.OpenSubKey(SUBKEY, true);
+                PermiteAlterar = true;
+            }
+            catch
+            {
+                registro = Registry.CurrentUser.OpenSubKey(SUBKEY);
+                PermiteAlterar = false;
+            }
+            DesligaEventos();
+            //Leitura inicial. Não ativar eventos
+            LeDados();
+            LigaEventos();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            //Feito para o caso do valor do NumericUpDown ter sido digitado, o que mantém o foco no controle e não executa _ValueChanged()
+            Focus();
+
+            registro.Close();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            LeDados();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            server[0] = textBox1.Text;
+            GravarProxyServer();
+            //Console.WriteLine("TextChanged");
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            server[1] = numericUpDown1.Value.ToString();
+            GravarProxyServer();
+            //Console.WriteLine("ValueChanged");
+        }
+    }
+}
